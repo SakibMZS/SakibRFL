307b3a17947f01e71fba8efbcc32034be19c0003
Syed Nazmus Sakib <afid.sakib@gmail.com>
2026-08-12 12:30:57 +0600

    Change initial sidebar state to 'locked'

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     page_title="Plastic-3 Operations Console | FF & GF",
     page_icon="🏭",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="locked",
 )
 
 
